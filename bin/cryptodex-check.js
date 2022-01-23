dcd7d3b31b1a12c581373a33d77aa96845516332
kimkykie <kimkiragu@gmail.com>
2022-01-23 20:39:32 +0300

    update: remove default values

--- bin/cryptodex-check.js
+++ bin/cryptodex-check.js
@@ -4,12 +4,8 @@ const check = require("../commands/check");
 program
   .command("portfolio")
   .description("Check portfolio value for tokens")
-  .option("--token <type>", "Add specific coin type", "BTC,ETH,XRP")
-  .option(
-    "--date <date>",
-    "Specify the date to check portfolio value",
-    Date.now()
-  )
+  .option("--token <type>", "Add specific coin type")
+  .option("--date <date>", "Specify the date to check portfolio value")
   .action((cmd) => check.portfolio(cmd));
 
 program.parse(process.argv);
