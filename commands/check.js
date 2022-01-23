5301b35d97d5b096672c042ea349fde741e66edd
kimkykie <kimkiragu@gmail.com>
2022-01-23 20:41:18 +0300

    update: add check calls for no parameter and token only provided

--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,20 @@
+const PortfolioAPI = require("../lib/PortfolioAPI");
+
 const check = {
-  portfolio(cmd) {
-    console.log(cmd);
+  async portfolio(cmd) {
+    try {
+      const api = new PortfolioAPI();
+      if (cmd.token === undefined && cmd.date === undefined) {
+        const portfolio = await api.getPortfolioNoParameter();
+        console.log(portfolio);
+      }
+      if (cmd.token !== undefined && cmd.date === undefined) {
+        const portfolio = await api.getPortfolioTokenOnly(cmd.token);
+        console.log(portfolio);
+      }
+    } catch (error) {
+      console.error(error.message.red);
+    }
   },
 };
 
