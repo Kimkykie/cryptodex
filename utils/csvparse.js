c7f1a5ce2afd7da3b3ff29daa93fbd684de27f72
kimkykie <kimkiragu@gmail.com>
2022-01-24 08:32:26 +0300

    update: add function to retrieve csv record from token and date

--- utils/csvparse.js
+++ utils/csvparse.js
@@ -1,5 +1,3 @@
-const { epoch } = require("./date");
-
 const readCSVDateOnly = (dateFromOption) => {
   return new Promise(function (resolve) {
     let csvOutput = [];
@@ -31,4 +29,38 @@ const readCSVDateOnly = (dateFromOption) => {
   });
 };
 
-module.exports = { readCSVDateOnly };
+const readCSVDateAndToken = (tokenOption, dateFromOption) => {
+  return new Promise(function (resolve) {
+    let csvOutput = [];
+    const lineReader = require("readline").createInterface({
+      input: require("fs").createReadStream("./assets/transactions.csv"),
+    });
+
+    lineReader.on("line", function (line) {
+      var jsonFromLine = {};
+      var lineSplit = line.split(",");
+
+      jsonFromLine.timestamp = lineSplit[0];
+      jsonFromLine.transaction_type = lineSplit[1];
+      jsonFromLine.token = lineSplit[2];
+      jsonFromLine.amount = lineSplit[3];
+
+      const d = new Date(jsonFromLine.timestamp * 1000);
+      const dateFromCSV =
+        d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
+
+      if (
+        dateFromCSV === dateFromOption &&
+        jsonFromLine.token.toUpperCase() === tokenOption
+      ) {
+        csvOutput.push(jsonFromLine);
+      }
+    });
+
+    lineReader.on("close", function (line) {
+      resolve(csvOutput);
+    });
+  });
+};
+
+module.exports = { readCSVDateOnly, readCSVDateAndToken };
