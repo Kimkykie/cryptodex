b3790f2811f20812ce613ac83714d60dee6fe869
kimkykie <kimkiragu@gmail.com>
2022-01-24 08:12:31 +0300

    update: add check for portfolio date only

--- commands/check.js
+++ commands/check.js
@@ -12,6 +12,10 @@ const check = {
         const portfolio = await api.getPortfolioTokenOnly(cmd.token);
         console.log(portfolio);
       }
+      if (cmd.token === undefined && cmd.date !== undefined) {
+        const portfolio = await api.getPortfolioDateOnly(cmd.date);
+        console.log(portfolio);
+      }
     } catch (error) {
       console.error(error.message.red);
     }
